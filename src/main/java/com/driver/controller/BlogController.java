84fd9212f44b11f04381c5fb944f2fa53687bf3f
AZMAT ALI <222azmat@gmail.com>
2023-02-18 17:15:08 +0530

    1

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -19,12 +19,12 @@ public class BlogController {
     private BlogRepository blogRepository;
 
     @PostMapping("/create")
-    public ResponseEntity<Void> createBlog(@RequestParam Integer userId ,
+    public ResponseEntity<Blog> createBlog(@RequestParam Integer userId ,
                                      @RequestParam String title,
                                      @RequestParam String content) {
         // Create a blog and add it under given user
-        blogService.createAndReturnBlog(userId,title,content);
-        return new ResponseEntity<>( HttpStatus.CREATED);
+
+        return new ResponseEntity<>(  blogService.createAndReturnBlog(userId,title,content),HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{blogId}")
